60ec3028f3bcea6dde548113206445c10aefc351
Cesar Morales <cesarmorales@cardioperfusion.com>
2025-03-04 12:37:49 -0500

    En vez de una web para el cliente, nos enfocaremos en una web administrativa interna primero

---
+++ src/routes/Router.js
@@ -0,0 +1,66 @@
+import { lazy } from "react";
+import { createBrowserRouter, Navigate } from "react-router";
+
+
+/****Layouts*****/
+const FullLayout = lazy(() => import("../layouts/FullLayout/FullLayout.js"));
+const BlankLayout = lazy(() => import('../layouts/blank/BlankLayout.js'));
+/****End Layouts*****/
+
+
+/*****Pages******/
+const Dashboard1 = lazy(() => import("../views/dashboards/Dashboard1.js"));
+
+/*****Tables******/
+const BasicTable = lazy(() => import("../views/tables/BasicTable.js"));
+
+// form elements
+const ExAutoComplete = lazy(() =>
+  import("../views/FormElements/ExAutoComplete.js")
+);
+const ExButton = lazy(() => import("../views/FormElements/ExButton.js"));
+const ExCheckbox = lazy(() => import("../views/FormElements/ExCheckbox.js"));
+const ExRadio = lazy(() => import("../views/FormElements/ExRadio.js"));
+const ExSlider = lazy(() => import("../views/FormElements/ExSlider.js"));
+const ExSwitch = lazy(() => import("../views/FormElements/ExSwitch.js"));
+const Error = lazy(() => import('../views/authentication/Error'));
+const Register = lazy(() => import('../views/authentication/Register.jsx'));
+const Login = lazy(() => import('../views/authentication/Login.jsx'));
+
+// form layouts
+const FormLayouts = lazy(() => import("../views/FormLayouts/FormLayouts.js"));
+
+/*****Routes******/
+
+const ThemeRoutes = [
+  {
+    path: "/",
+    element:<FullLayout/>,
+    children: [
+      { path: "/", element: <Navigate to="dashboards/dashboard1" /> },
+      { path: "dashboards/dashboard1", exact: true, element: <Dashboard1 /> },
+      { path: "tables/basic-table", element: <BasicTable /> },
+      { path: "/form-layouts/form-layouts", element: <FormLayouts /> },
+      { path: "/form-elements/autocomplete", element: <ExAutoComplete /> },
+      { path: "/form-elements/button", element: <ExButton /> },
+      { path: "/form-elements/checkbox", element: <ExCheckbox /> },
+      { path: "/form-elements/radio", element: <ExRadio /> },
+      { path: "/form-elements/slider", element: <ExSlider /> },
+      { path: "/form-elements/switch", element: <ExSwitch /> },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <BlankLayout />,
+    children: [
+      { path: '404', element: <Error /> },
+      { path: '/auth/register', element: <Register /> },
+      { path: '/auth/login', element: <Login /> },
+      { path: '*', element: <Navigate to="/auth/404" /> },
+    ],
+  },
+];
+
+const router = createBrowserRouter(ThemeRoutes);
+
+export default router;
